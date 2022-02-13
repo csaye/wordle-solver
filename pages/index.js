d160fd51654c87541ead13f4b95f3f32585afce4
Cooper Saye <coopersaye@gmail.com>
2022-02-12 20:05:58 -0800

    Display word.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  const [word, setWord] = useState('');
+
   // called when key pressed
   function onKeydown(e) {
-    const key = e.key.toLowerCase();
-    console.log(key);
+    const key = e.key.toUpperCase();
+    if (/^[A-Z]$/.test(key)) setWord(val => val + key);
+    if (key === 'ENTER') setWord('');
+    if (key === 'BACKSPACE') setWord(val => val.slice(0, -1));
   }
 
   // listen for keypresses
@@ -17,6 +21,7 @@ export default function Index() {
 
   return (
     <div>
+      <h1>{word}</h1>
     </div>
   );
 }
