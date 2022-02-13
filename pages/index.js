56c3493f4d34a798118ca9f58fa60ef7a68bc67f
Cooper Saye <coopersaye@gmail.com>
2022-02-12 20:27:59 -0800

    Fix key handling.

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,16 @@ export default function Index() {
   // called when key pressed
   function onKeydown(e) {
     const key = e.key.toUpperCase();
-    if (/^[A-Z]$/.test(key)) setWord(val => val + key);
-    if (key === 'ENTER') setWord('');
-    if (key === 'BACKSPACE') setWord(val => val.slice(0, -1));
+    if (/^[A-Z]$/.test(key) && word.length < 5) setWord(val => val + key);
+    if (key === 'ENTER' && word.length == 5) setWord('');
+    if (key === 'BACKSPACE' && word.length > 0) setWord(val => val.slice(0, -1));
   }
 
   // listen for keypresses
   useEffect(() => {
     window.addEventListener('keydown', onKeydown);
     return () => window.removeEventListener('keydown', onKeydown);
-  }, []);
+  }, [onKeydown]);
 
   return (
     <div className={styles.container}>
