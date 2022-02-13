ae8dfc57460ad062993c81858adf9fdccdd28618
Cooper Saye <coopersaye@gmail.com>
2022-02-12 20:05:52 -0800

    Listen for keypresses.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
+import { useEffect } from 'react';
+
 import styles from '../styles/pages/Index.module.css';
 
 export default function Index() {
+  // called when key pressed
+  function onKeydown(e) {
+    const key = e.key.toLowerCase();
+    console.log(key);
+  }
+
+  // listen for keypresses
+  useEffect(() => {
+    window.addEventListener('keydown', onKeydown);
+    return () => window.removeEventListener('keydown', onKeydown);
+  }, []);
+
   return (
     <div>
     </div>
